300eeb3be117a1b61736c940df3a57efe9fa7d31
alexdmacon <alexdmacon@gmail.com>
2022-01-10 20:16:39 -0600

    implemented apollo server and applied it to express as middleware

--- server/server.js
+++ server/server.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
+
 const db = require('./config/connection');
+
+// will be replaced by graphQL API
 const routes = require('./routes');
+// will be replaced by graphQL API
+
+// still need to create
+const { typeDefs, resolvers } = require('./schemas');
+// still need to create
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,7 +30,14 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+// will be replaced by graphQL API
 app.use(routes);
+// will be replaced by graphQL API
+
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
