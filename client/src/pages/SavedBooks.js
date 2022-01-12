8bdae7bcf07e3d1d3e41d748e57efbc3f9ba07cd
alexdmacon <alexdmacon@gmail.com>
2022-01-11 19:39:29 -0600

    refactored login and signup forms

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -5,12 +5,16 @@ import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
+
+import { useQuery, useMutation } from '@apollo/client';
 import { GET_ME } from '../utils/queries'
 import { REMOVE_BOOK } from '../utils/mutations'
 
 
 const SavedBooks = () => {
   const [removeBook, { error }] = useMutation(REMOVE_BOOK);
+  const { loading, data } = useQuery(QUERY_ME);
+
 
   // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
