c1e0e04f73cb37f6cdc7a83c143a18732ec01d03
alexdmacon <alexdmacon@gmail.com>
2022-01-11 20:46:44 -0600

    still troubleshooting

--- server/server.js
+++ server/server.js
@@ -1,26 +1,19 @@
 const express = require('express');
 const path = require('path');
-const { ApolloServer } = require('apollo-server-express');
-
 const db = require('./config/connection');
-
-// will be replaced by graphQL API
-const routes = require('./routes');
-// will be replaced by graphQL API
-
-// still need to create
+const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
-// still need to create
-
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
 
-server.applyMiddleware({ app });
+server.applyMiddleware({ app: server });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -34,11 +27,9 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-// will be replaced by graphQL API
-app.use(routes);
-// will be replaced by graphQL API
-
-
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
