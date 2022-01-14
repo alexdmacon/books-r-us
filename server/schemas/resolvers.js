7d83844ae1e63b681727c23ebe94cd4a30aad4c2
alexdmacon <alexdmacon@gmail.com>
2022-01-13 19:14:17 -0600

    fixed small bugs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,19 +1,20 @@
-const { User } = require('../models');
-const { AuthenticationError } = require('apollo-server-express');
-const { signToken } = require('../utils/auth');
+const { User } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
-      if(context.user) {
-        const userData = await User.findOne({ _id: context.user._id })
-          .select('-__v -password')
-      
-      return userData;
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
+
+        return userData;
       }
 
-      throw new AuthenticationError('Not logged in');
-    }
+      throw new AuthenticationError("Not logged in");
+    },
   },
 
   Mutation: {
@@ -28,13 +29,13 @@ const resolvers = {
       const user = await User.findOne({ email });
 
       if (!user) {
-        throw new AuthenticationError('Incorrect credentials');
+        throw new AuthenticationError("Incorrect credentials");
       }
-      
+
       const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
+        throw new AuthenticationError("Incorrect credentials");
       }
 
       const token = signToken(user);
@@ -49,7 +50,7 @@ const resolvers = {
         );
         return updatedUser;
       }
-      throw new AuthenticationError('You need to be logged in!')
+      throw new AuthenticationError("You need to be logged in!");
     },
 
     removeBook: async (parent, args, context) => {
@@ -61,9 +62,9 @@ const resolvers = {
         );
         return updatedUser;
       }
-      throw new AuthenticationError('You need to be logged in!')
-    } 
-  }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
 };
 
 module.exports = resolvers;
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,13 +1,13 @@
-const jwt = require('jsonwebtoken');
-const secret = 'notsosecret';
-const expiration = '2h';
+const jwt = require("jsonwebtoken");
+const secret = "notsosecret";
+const expiration = "2h";
 
 module.exports = {
   authMiddleware: function ({ req }) {
     let token = req.body.token || req.query.token || req.headers.authorization;
 
     if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+      token = token.split(" ").pop().trim();
     }
 
     if (!token) {
@@ -18,7 +18,7 @@ module.exports = {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
-      console.log('Invalid token');
+      console.log("Invalid token");
     }
 
     return req;
